692b6674239e5272d169c0488bc42d81bb391b94
braqn1999 <117543750+braqn1999@users.noreply.github.com>
2025-02-17 21:53:49 -0600

    Day1: Add list and type (initial commit)

---
+++ main.py
@@ -0,0 +1,11 @@
+name_car = "Enter your car: "
+
+text1 = input(name_car)
+text2 = input(name_car)
+text3 = input(name_car)
+
+list_cars = [text1, text2, text3]
+
+print(list_cars)
+
+print(type(list_cars))
