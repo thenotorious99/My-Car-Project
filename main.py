b437b84a021516f870c4a1921faf40b5e680a80d
braqn1999 <117543750+braqn1999@users.noreply.github.com>
2025-02-22 10:15:41 -0600

    Day5: Add complete, add enumarate and checking the numbers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 list_cars = []
 
 while True:
-    user_action = input("Add new car, show, edit or exit:")
+    user_action = input("Add new car, show, edit, complate or exit:")
     name_car = user_action.strip()
 
     match name_car:
@@ -10,13 +10,16 @@ while True:
             car_name = input("Enter a car: ")
             list_cars.append(car_name)
         case 'show':
-            for item in list_cars:
-                print(item)
+            for index, item in enumerate(list_cars):
+                print(f"{index + 1}-{item}")
         case 'edit':
             number = int(input("Number of the car to edit: "))
             number -= 1
             new_car = input("Enter new car: ")
             list_cars[number] = new_car
+        case 'complete':
+            number = int(input("Number of the car to complete: "))
+            list_cars.pop(number)
         case 'exit':
             break
 
