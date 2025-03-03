3658aae026701ba6569a5ce8cd02e9df61c01893
braqn1999 <117543750+braqn1999@users.noreply.github.com>
2025-03-02 21:26:05 -0600

    Day11: add def function and more dataframe and txt filee

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from pyexpat.errors import messages
-
-
 def get_car():
-    with open('cars.txt', 'r') as file:
-        cars = file.readlines()
+    with open('cars.txt', 'r') as file_local:
+        cars = file_local.readlines()
     return cars
 
 while True:
-    user_action = input("Add new car, show, edit, complate or exit:")
+    user_action = input("Add new car, show, edit, complete or exit: ")
     name_car = user_action.strip()
 
 
-    if user_action.startswith('add'):
+    if user_action.startswith("add"):
         car_name = user_action[4:]
 
         cars = get_car()
 
         cars.append(car_name + '\n')
 
-    elif user_action.startswith('show'):
+        with open('cars.txt', 'w') as file:
+            file.writelines(cars)
+
+    elif user_action.startswith("show"):
+
         cars = get_car()
 
         for index, item in enumerate(cars):
@@ -27,7 +28,7 @@ while True:
             print(row)
 
 
-    elif user_action.startswith('edit'):
+    elif user_action.startswith("edit"):
        try:
 
             number = int(user_action[5:])
@@ -46,20 +47,28 @@ while True:
        except ValueError:
            print("Your command is not valid!")
            continue
-    elif user_action.startswith('complete'):
-        number = int(user_action[9:])
 
-        cars = get_car()
-        index = number - 1
-        cars_remove = cars[index].strip('\n')
-        cars.pop(index)
+    elif user_action.startswith("complete"):
+        try:
 
-        with open('cars.txt', 'w') as file:
-            file.writelines(cars)
+            number = int(user_action[9:])
 
-        messages = f" This car {cars_remove} was remove from the list."
-        print(messages)
-    elif user_action.startswith('exit'):
+            cars = get_car()
+
+            index = number - 1
+            cars_remove = cars[index].strip('\n')
+            cars.pop(index)
+
+            with open('cars.txt', 'w') as file:
+                file.writelines(cars)
+
+            messages = f" This car {cars_remove} was remove from the list."
+            print(messages)
+        except ValueError:
+            print("There is no car with that number")
+            continue
+
+    elif user_action.startswith("exit"):
         break
     else:
         print("Command isn't valid.")
